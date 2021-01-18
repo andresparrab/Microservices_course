635d30cea426df725a6904fd36c97e0a9f55a222
andresparrab <andres.parrab@gmail.com>
2021-01-18 19:44:51 +0100

    2021-01-17

--- typescript/mapsApp/src/index.ts
+++ typescript/mapsApp/src/index.ts
@@ -1,1 +1,24 @@
-console.log("hi there");
+// import { User } from "./User";
+// import { Company } from "./Company";
+import "googlemaps";
+// const user = new User();
+// console.log(user);
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(document.getElementById("mymap"), {
+  zoom: 15,
+  center: {
+    lat: 50,
+    lng: 10,
+  },
+});
+
+let map: google.maps.Map;
+
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
